4feb617fd0fc33fed79e7768fbd1d664851a5002
Sebastian Arias <sebasariasporras@gmail.com>
2023-06-20 14:44:07 -0600

    agregar ultimos arreglos

--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,16 @@
 class Contador {
-  // Inserte el código aquí
+  valor = 0;
+  siguiente() {
+    this.valor += 1;
+  }
 }
-
 // Inserte el código aquí
+const instancia = new Contador();
+// console.log(instancia)
 
-export { instancia }
+instancia.siguiente();
+console.log(instancia);
+instancia.siguiente();
+console.log(instancia);
+
+export { instancia };
---
+++ src/prueba.js
@@ -0,0 +1,69 @@
+
+
+function Sumar(...numeros) {
+  return numeros.reduce(// function introducida 
+(acumulador, valorActual) => {
+    return acumulador + valorActual;
+  }, 0);
+}
+const resultado = Sumar(1, 2, 3, 4, 5, 10);
+console.log({ resultado });
+
+
+
+// let lista = [1, 2, 3, 4];
+
+// let impirmirLista = (miLista) => {
+//   let nuevaLista = [...miLista];
+//   miLista.push(5, 6, 7);
+//   console.log(miLista);
+// };
+// console.log(lista);
+// impirmirLista(lista);
+// FILTER FUNCTION
+
+// function esCarro(elementoActual) {
+//   return elementoActual === "carrorojo";
+// }
+
+// function noEsCarro(elementoActual) {
+//   return elementoActual != "carrorojo";
+// }
+
+// let vehiculos = ["carrorojo", "carrorojo", "taxi", "bus", "tren"];
+// let listaCarros = vehiculos.filter(esCarro);
+// let otrosVehiculos = vehiculos.filter(noEsCarro);
+// console.log(listaCarros);
+// console.log(otrosVehiculos);
+
+// MAP FUNCTION
+
+// function hacerFiletes(elementoActual) {
+//   let respuesta = "";
+//   if (elementoActual === "vaca") {
+//     respuesta = "filet de res";
+//   } else if (elementoActual == "pescado") {
+//     respuesta = "filet de pescado";
+//   } else {
+//     respuesta = "no es";
+//   }
+//   return respuesta;
+// }
+
+// let filetes = ["vaca", "vaca", "pescado", "pescado", "vaca", "kiwii"].map(
+//   hacerFiletes
+// );
+// console.log(filetes);
+
+// REDUCE FUNCTION
+// function sumaTotal(acumulador, valorActual) {
+//     console.log()
+//     console.log({acumulador})
+//     console.log({valorActual})
+//     let sumaActual = acumulador + valorActual
+//     console.log({sumaActual})
+//     return sumaActual;
+// }
+
+// let Suma = [1,2,3,4,5].reduce(sumaTotal, 0)
+// console.log(Suma)
