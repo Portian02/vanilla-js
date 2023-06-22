6bb4aaea66e6a1ab3eadfd089cf651bf62d64a50
Sebastian Arias <sebasariasporras@gmail.com>
2023-06-22 13:07:53 -0600

    ejercicios

--- src/index.js
+++ src/index.js
@@ -1,50 +1,56 @@
 // 1. Escribir un código que modifique el HTML y cada segundo muestre la hora en un elemento de HTML.
 
 // Inserte el código aquí
-let time = document.getElementById("#Hora");
+
+let time = document.querySelector("#Hora");
 
 let digitalClock = (date) => {
   let hora = date.getHours();
   let minutos = date.getMinutes();
-  let segundo = date.getSeconds();
-
-  console.log(hora + minutos + segundo);
-  return hora + ":" + minutos + ":" + segundo;
+  let segundos = date.getSeconds();
+  return hora + ":" + minutos + ":" + segundos;
 };
 
-const updateclock = () => {
+const updateClock = () => {
   const date = new Date();
-  const horaActual = getCurrentTime(date);
+  const horaActual = digitalClock(date);
   time.textContent = horaActual;
 };
-updateclock();
-setInterval(updateclock(), 1000);
+
+updateClock();
+setInterval(updateClock, 1000);
 
 //1. Escribir una función llamada obtenerUsuarios utilizando promesas.
 //    * En el package.json existe un script para levantar un servidor local. Ejecutelo y navegue a http://localhost:3000/users
 //    * Utilizando la función Fetch, debe llamar al url http://localhost:3000/users y obtener la lista de usuarios.
 //    * Una vez obtenga la lista de usuarios, imprimalos en la consola.
 //    * Invoque la función para que se ejecute el llamado al servidor.
-const imprimirUsuarios = () => {
-  const usuarios = obtenerUsuarios();
-  usuarios
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Error en respuesta");
-      }
-    })
-    .then((usuarios) => {
-      console.log(usuarios);
+// Inserte el código aquí
+
+obtenerListaUsuarios();
+function obtenerListaUsuarios() {
+  //hacer una función//
+
+  fetch("http://localhost:3000/users")
+    .then((respuesta) => respuesta.json())
+    .then((datos) => {
+      console.log("Lista de usuarios:", datos);
     })
     .catch((error) => {
-      console.log(error);
+      console.log("Error al obtener la lista de usuarios:", error);
     });
-};
 
-// Inserte el código aquí
+  function usuarios() {
+    return new Promise(obtenerListaUsuarios);
+  }
+}
 
 // 1. Escribir una función llamada obtenerUsuarios.
 //    * Modificar la función para que utilice async/await
 // Inserte el código aquí
+async function datosUsuarios() {
+  const datosdelUsuario = await obtenerListaUsuarios();
+  console.log(datosdelUsuario);
+}
+
+datosUsuarios();
