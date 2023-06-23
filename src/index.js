caf8ac273d76e2e6da30034bee8d2b2c97bbd345
Sebastian Arias <sebasariasporras@gmail.com>
2023-06-23 15:58:38 -0600

    agregar reloj digital

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-let time = document.querySelector("#Hora");
+let time = document.querySelector("#hour");
+let fecha = document.querySelector("#date");
 
 let digitalClock = (date) => {
   let hora = date.getHours();
   let minutos = date.getMinutes();
+  minutos = ("0" + minutos).slice(-2);
   let segundos = date.getSeconds();
-  return hora + ":" + minutos + ":" + segundos;
+  segundos = ("0" + segundos).slice(-2);
+  let jornada = hora >= 12 ? 'PM':'AM'
+  return hora + ":" + minutos + ":" + segundos + "  " + jornada;
 };
 
 const updateClock = () => {
@@ -13,5 +17,18 @@ const updateClock = () => {
   time.textContent = horaActual;
 };
 
-updateClock();
+// let fechaActual = (Date) => {
+//   let dia = Date.getDate();
+//   let mes = Date.getMonth() + 1;
+//   let año = Date.getFullYear();
+//   let
+//   return dia + "/" + mes + "/" + año;
+// };
+
+// const updateDate = () => {
+//   const date = new Date();
+//   const fecha = updateClock(date);
+//   fecha.textContent = fecha;
+// };
+
 setInterval(updateClock, 1000);
